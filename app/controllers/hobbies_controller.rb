8dac3639d385cd00f2e41b2130cba72f72b22940
Nathan Kane <kaneitalia.rana@gmail.com>
2020-11-23 16:00:33 -0800

    added ability to add a new hobby

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,4 +1,7 @@
 class HobbiesController < ApplicationController
+
+  before_action :set_hobby, only: [:show, :edit, :update]
+
   def index
     @hobbies = Hobby.all
   end
@@ -9,6 +12,44 @@ class HobbiesController < ApplicationController
   def edit
   end
 
-  def new
+  def update
+    if @hobby.update(hobby_params)
+      redirect_to hobbies_path
+    else
+      render :edit
+    end
   end
+
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    # one way to create an account does not santize
+    # current_user.accounts.create(name: params[:account][:name], balance: params[:account][:balance])
+    @hobby = current_user.hobbies.new(hobby_params) #create this
+    if @hobby.save
+      # flash[:success] = "Account Created"
+      redirect_to hobbies_path
+    else
+      # flash[:error] = "Error #{@account.errors.full_messages.join(", ")}"
+      render :new
+    end
+  end
+
+  def destroy
+    @hobby.destroy
+    redirect_to hobbies_path
+  end
+
+  private
+
+  def hobby_params
+    params.require(:hobby).permit(:name, :body)
+  end
+
+  def set_hobby
+    @hobby = current_user.hobbies.find(params[:id])
+  end
+
 end
